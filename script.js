8e20848c583d42ac7f1ac09e5cb5f33f8caead8d
Athiqur Rahman <athiqurrahman3170@gmail.com>
2021-08-05 21:41:23 -0400

    Fix decimal calculations

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let equationArray = []
 let pressedEqual = false;
 let pressedRemoved = false;
 let numberClickedEqual = 0;
+let signOccurences = 0
+
 
 /*If a button besides CLEAR AND DELETE are clicked, the text from that button is appended to the equation 
 which shows on top left of screen.*/
@@ -20,17 +22,11 @@ buttons.forEach((button) => {
         if ((button.innerText != "CLEAR") && (button.innerText !="DELETE")){
             if (pressedEqual == true) {
                 if (button.innerText != '÷' && button.innerText != 'x' && button.innerText != '-' && button.innerText != '+' ) {
-                    topLeftText.innerText = "";
-                    bottomRightText.innerText = "";
-                    equation = "";
-                    answer = "";
-                    equationArray = [];
-
+                    reset();
             }}
             if (button.innerText != '÷' && button.innerText != 'x' && button.innerText != '-' && button.innerText != '+' && button.innerText != '=' ) {
                 temp += button.innerText;
                 bottomRightText.innerText = temp;
-                numberClickedEqual ++
             }
             else if (button.innerText == '÷' || button.innerText == 'x' || button.innerText == '-' || button.innerText == '+' ) {
                 bottomRightText.innerText = temp;
@@ -38,8 +34,8 @@ buttons.forEach((button) => {
             }
             else if (button.innerText == '='){
                 temp = ""
+                numberClickedEqual++
             }
-
             equation += button.innerText;
             equationArray.push(button.innerText);
             topLeftText.innerText = equation;
@@ -57,31 +53,31 @@ equalButton.addEventListener('click', () => {
     pressedEqual = true
 
     if (pressedEqual) {
-        if (numberClickedEqual == 0) {
-            answer += calculate(equationArray);
-            if (answer > 1000000000){
-                answer = answer / 100
-            }
-            equation += answer;
+        //Array starts with the previous answer when user clicks on a number immediately after clicking on the equal sign
+        if (numberClickedEqual >= 3) {
+            equationArray = equationArray.slice(equationArray.indexOf('=') + 1);
         }
-        else {
-            answer += calculate(equationArray);
-            equation += answer;
-            console.log(equationArray)
+        answer += calculate(equationArray);
 
+        //Outputs answer in scientific notation 
+        if (answer > 1000000000){
+            answer = parseFloat(answer).toExponential(5);
         }
 
+        equation += answer;
         equationArray.push(answer)
         console.log(answer);
         //Answer to the equation shows up on the bottom right
         bottomRightText.innerText = answer;
+
         //Full equation with answer shows on top left
         topLeftText.innerText = equation;
+
         //This is so future calculations can be done
         equation = answer;
         answer = "";
-        numberClickedEqual += 1;
-
+        signOccurences = 0;
+        numberClickedEqual++;
     }
 })
 
@@ -89,29 +85,39 @@ equalButton.addEventListener('click', () => {
 //If CLEAR or DELETE are clicked, the topleft and bottomright of screen are reset.
 for (let i = 0; i < remove.length; i++) {
     remove[i].addEventListener('click',() => {
-        topLeftText.innerText = "";
-        bottomRightText.innerText = "";
-        temp = ""
-        equation = "";
-        answer = ""
-        equationArray = [];
-        pressedRemoved = true;
-        pressedEqual = false;
+        reset();
     });
+}
+
+function reset() {
+    topLeftText.innerText = "";
+    bottomRightText.innerText = "";
+    temp = ""
+    equation = "";
+    answer = ""
+    equationArray = [];
+    pressedRemoved = true;
+    pressedEqual = false;
+    numberClickedEqual = 0;
 }
 
 /*Used to join adjacent elements together if they are both numbers 
 in order to perform calculations for numbers with more than 1 digit. */
 function format(arr){
     for (let j = arr.length - 1; j >= 0; --j){
+        console.log(arr)
         if (Number.isInteger(parseInt(arr[j])) && Number.isInteger(parseInt(arr[j-1]))) {
             arr[j-1] += arr[j];
-            arr.splice(j, 1)
+            arr.splice(j, 1);
         }
-        else if (arr[j] == '.') {
+        if (arr[j] == '.' && (arr[j-1] != "+" && arr[j-1] != "-" && arr[j-1] != "x" && arr[j-1] != "÷" && arr[j-1] != "=")) {
             arr[j-1] = arr[j-1] + '.' + arr[j+1];
-            arr.splice(j+1, 1)
-            arr.splice(j, 1)
+            arr.splice(j+1, 1);
+            arr.splice(j, 1);
+        }
+        if (arr[j] == '.' && (arr[j-1] == "+" || arr[j-1] == "-" || arr[j-1] == "x" || arr[j-1] == "÷" || arr[j-1] == "=")) {
+            arr[j] = "0" + '.' + arr[j+1];
+            arr.splice(j+1, 1);
         }
     }
 }
@@ -119,27 +125,35 @@ function format(arr){
 function calculate(arr) {
     
     format(arr);
-  
-    // console.log(arr)
-    // console.log(newArr)
-
+    console.log(arr)
     //Performs the actual calculations by looping through the array and rounding to 8 decimal points
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] == "+") {
-            return Math.round((parseFloat(arr[i-1]) + parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
+    for (let h = 0; h < arr.length; h++){
+        if (arr[h] == "+" || arr[h] == "-" || arr[h] == "x" || arr[h] == "÷" || arr[h] == "=") {
+            signOccurences++;
         }
-        else if (arr[i] == "-") {
-            return Math.round((parseFloat(arr[i-1]) - parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
-        }
-        else if (arr[i] == "x") {
-            return Math.round((parseFloat(arr[i-1]) * parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
-        }
-        else if (arr[i] == "÷") {
-            return Math.round((parseFloat(arr[i-1]) / parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
-        }
+
     }
+    console.log(signOccurences)
+    return computate(arr, signOccurences)
 }
 
+function computate(arr, signOccurences) {
+    
+    for (let i = 0; i < arr.length; i++){
+        // if (signOccurences == 2) {
+            if (arr[i] == "+") {
+                return Math.round((parseFloat(arr[i-1]) + parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
+            }
+            else if (arr[i] == "-") {
+                return Math.round((parseFloat(arr[i-1]) - parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
+            }
+            else if (arr[i] == "x") {
+                return Math.round((parseFloat(arr[i-1]) * parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
+            }
+            else if (arr[i] == "÷") {
+                return Math.round((parseFloat(arr[i-1]) / parseFloat(arr[i+1]) + Number.EPSILON)* 100000000) / 100000000;
+            }
+        // }
+}}
 
-//Need to work on allowing for multiple operations in one line. Ex 2+3+4+5-14. Can only do 2+3=5
-//Need to work on calculations after = sign
+
